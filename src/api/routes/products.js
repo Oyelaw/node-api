c4d438b358ab44dc9c8ec7c7acafa25e2d4cfc24
okiki <okiki.oyelaw@gmail.com>
2018-08-20 15:56:49 +0100

    set up validation

--- src/api/routes/products.js
+++ src/api/routes/products.js
@@ -1,17 +1,59 @@
 const express = require('express');
 const router = express.Router();
 const mongoose = require('mongoose');
+const multer = require('multer');
+
+const storage = multer.diskStorage({
+  destination: function(req, file, cb) {
+    cb(null, './uploads/');
+  },
+  filename: function(req, file, cb) {
+    cb(null, new Date().toISOString() + file.originalname);
+  }
+});
+
+const fileFilter = (req, file, cb) => {
+  // reject a file
+  if (file.mimetype === 'image/jpeg' || file.mimtype == 'image/png') {
+      cb(null, true);
+  } else {
+      cb(null, false);
+  }
+}
+
+const upload = multer({
+  storage: storage,
+  limits: {
+    fileSize: 1024 * 1024 * 5
+  },
+  fileFilter: fileFilter
+});
 
 const Product = require('../models/product');
 
 
 router.get('/', (req, res, next) => {
   Product.find()
+      .select('name price _id productImage')
       .exec()
       .then(docs => {
-        console.log(docs);
+          const response = {
+            count: docs.length,
+            products: docs.map(doc => {
+              return {
+                name: doc.name,
+                price: doc.price,
+                productImage: doc.productImage,
+                _id: doc._id,
+                request: {
+                  type: 'GET',
+                  url: 'http://localhost:3000/products/' + doc._id
+                }
+              }
+            })
+          }
         // if (docs.length > 0) {
-          res.status(200).json(docs);
+          res.status(200).json(response);
         // } else {
         //   res.status(404).json({
         //     message: 'No entries found'
@@ -30,11 +72,19 @@ router.get('/', (req, res, next) => {
 router.get('/:productId', (req, res, next) => {
     const id = req.params.productId;
     Product.findById(id)
+      .select('name price _id productImage')
       .exec()
       .then(doc => {
         console.log('from database', doc);
         if (doc) {
-          res.status(200).json(doc)
+          res.status(200).json({
+            product: doc,
+            request: {
+              type: 'GET',
+              description: 'Get all products',
+              url: 'http://localhost:3000/products'
+            }
+          })
         } else {
           res.status(404).json({message: 'No Valid entry found for provided ID'})
         }
@@ -55,8 +105,10 @@ router.patch('/:productId', (req, res, next) => {
     Product.update({ _id: id }, { $set: updateOps })
         .exec()
         .then(result => {
-          console.log(result);
-          res.status(200).json(result)
+          res.status(200).json({
+            message: 'product updated',
+            request: 'http://localhost:3000/products/' + id
+          })
         })
         .catch(err => {
           console.log(err);
@@ -71,7 +123,14 @@ router.delete('/:productId', (req, res, next) => {
     Product.remove({ _id: id })
         .exec()
         .then(result => {
-          res.status(200).json(result)
+          res.status(200).json({
+            message: 'product deleted',
+            request: {
+              type: 'POST',
+              url: 'http://localhost:3000/products',
+              body: { name: 'String', price: 'Number' }
+            }
+          })
         })
         .catch(err => {
           console.log(err);
@@ -81,12 +140,13 @@ router.delete('/:productId', (req, res, next) => {
         });
 });
 
-router.post('/', (req, res, next) => {
-
+router.post('/', upload.single('productImage'), (req, res, next) => {
+  console.log(req.file);
   const product = new Product({
     _id: new mongoose.Types.ObjectId(),
     name: req.body.name,
-    price: req.body.price
+    price: req.body.price,
+    productImage: req.file.path
   });
   product
     .save()
@@ -94,7 +154,15 @@ router.post('/', (req, res, next) => {
     console.log(result);
     res.status(201).json({
       message: 'Handling POST request to /products',
-      createdProduct: product
+      createdProduct: {
+        name: result.name,
+        price: result.price,
+        _id: result._id,
+        request: {
+          type: 'GET',
+          url: 'http://localhost:3000/products/' + result._id
+        }
+      }
     });
   })
   .catch(err => {
